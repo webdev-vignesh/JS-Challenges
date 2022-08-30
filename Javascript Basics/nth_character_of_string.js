fcfcf5b1b7a5638fb18a1e112c2d7d11e34d2fb2
web3 vicky <vicky.web3@gmail.com>
2022-08-30 22:37:37 +0530

    finding nth character of string

---
+++ Javascript Basics/nth_character_of_string.js
@@ -0,0 +1,10 @@
+// Write a function that takes a string (a) and a number (n) as argument
+// Return the nth character of 'a'
+
+function myFunction(a, n) {
+    return a[n];
+}
+
+console.log(myFunction('Hello World!', 6));
+console.log(myFunction('gfedcba',3));
+console.log(myFunction('abcd',1));
