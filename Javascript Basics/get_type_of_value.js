614282f42fbb82cbc0f0bf1fe941ac8ee6ea2bb9
web3 vicky <vicky.web3@gmail.com>
2022-08-30 22:33:14 +0530

    finding datatype of a value

---
+++ Javascript Basics/get_type_of_value.js
@@ -0,0 +1,10 @@
+// Write a function that takes a value as argument
+// Return the type of the value
+
+function myFunction(a) {
+    return typeof a;
+}
+
+console.log(myFunction(3));
+console.log(myFunction('JavaScript'));
+console.log(myFunction(true));
