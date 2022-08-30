b13bc05345bbbddbecadb5fe9b50d0a8761c4cf9
web3 vicky <vicky.web3@gmail.com>
2022-08-30 22:29:07 +0530

    comparing 2 values are equal

---
+++ Javascript Basics/comparison_operators.js
@@ -0,0 +1,11 @@
+// Write a function that takes two values, say a and b, as arguments
+// Return true if the two values are equal and of the same type
+
+function myFunction(a, b) {
+    return a === b; 
+}
+
+console.log(myFunction(2, 2));
+console.log(myFunction(7,'hello'));
+console.log(myFunction(22.5, 23));
+console.log(myFunction(16, 16));
