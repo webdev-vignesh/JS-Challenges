d17e822874ddd2614342c03e618447a20716b42a
web3 vicky <vicky.web3@gmail.com>
2022-08-31 19:57:44 +0530

    find b percent of a

---
+++ Javascript Basics/return_percentage_of_a_number.js
@@ -0,0 +1,9 @@
+// Write a function that takes two numbers (a and b) as argument
+// Return b percent of a
+function myFunction (a, b) {
+    return (b/a)*100;
+}
+
+console.log(myFunction(100,50));
+console.log(myFunction(10,1));
+console.log(myFunction(500,25));
