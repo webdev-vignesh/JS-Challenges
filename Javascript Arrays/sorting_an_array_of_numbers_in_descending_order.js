1871e581b131af9d46fbc686dd5dda80dd3000d1
web3 vicky <vicky.web3@gmail.com>
2022-09-08 13:28:07 +0530

    array sorting descending order

---
+++ Javascript Arrays/sorting_an_array_of_numbers_in_descending_order.js
@@ -0,0 +1,10 @@
+// Write a function that takes an array of numbers as argument
+// It should return an array with the numbers sorted in descending order
+
+function myFunction(arr) {
+    return arr.sort().reverse();
+}
+
+
+console.log(myFunction([1,3,2]));
+console.log(myFunction([4,2,3,1]));
