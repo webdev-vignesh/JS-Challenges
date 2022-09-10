5005ece6a2004fe56348bef27e3d0c854c4d40f8
web3 vicky <vicky.web3@gmail.com>
2022-09-10 16:31:56 +0530

    commit

---
+++ Javascript Basics/check_if_a_number_is_even.js
@@ -0,0 +1,12 @@
+// Write a function that takes a number as argument
+// If the number is even, return true
+// Otherwise, return false
+
+function myFunction(a) {
+    return a % 2 === 0; 
+}
+
+console.log(myFunction(10));
+console.log(myFunction(-4));
+console.log(myFunction(5));
+console.log(myFunction(-111));
